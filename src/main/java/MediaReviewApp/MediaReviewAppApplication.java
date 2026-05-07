0a87d569f8144bfccbbd04ba123a845b3e2246de
daigo-fujiwara <skniksknsn@gmail.com>
2026-05-07 12:09:01 +0900

    Spring Initializrによるプロジェクト作成

---
+++ src/main/java/MediaReviewApp/MediaReviewAppApplication.java
@@ -0,0 +1,13 @@
+package MediaReviewApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaReviewAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaReviewAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MediaReviewApp/MediaReviewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package MediaReviewApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaReviewAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
