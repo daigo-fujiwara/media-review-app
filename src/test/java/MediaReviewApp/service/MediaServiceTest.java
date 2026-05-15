64f83a9531a1c2e5f37eaad89a7a2d62c54eaa43
daigo-fujiwara <skniksknsn@gmail.com>
2026-05-15 17:21:09 +0900

    テスト変更

--- src/test/java/MediaReviewApp/service/MediaServiceTest.java
+++ src/test/java/MediaReviewApp/service/MediaServiceTest.java
@@ -3,18 +3,15 @@ package MediaReviewApp.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import MediaReviewApp.entity.Media;
 import MediaReviewApp.repository.MediaRepository;
 
